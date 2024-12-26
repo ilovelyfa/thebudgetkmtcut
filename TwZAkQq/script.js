24e39b41251eb3b5e054163c27a12d0d3659feec
junsuwhy <junsuwhy@gmail.com>
2024-12-26 23:47:59 +0800

    Upload TwZAkQq

---
+++ TwZAkQq/script.js
@@ -0,0 +1,111 @@
+function App() {
+  const scrollToAchievements = () => {
+    document.querySelector('.achievements').scrollIntoView({
+      behavior: 'smooth' });
+
+  };
+
+  return /*#__PURE__*/(
+    React.createElement("div", null, /*#__PURE__*/
+
+    React.createElement("header", null, /*#__PURE__*/
+    React.createElement("div", { className: "container banner" }, /*#__PURE__*/
+    React.createElement("img", { src: "images/building_gyousei_kokuzeichou.png", alt: "Logo" }), /*#__PURE__*/
+    React.createElement("h1", null, "2025\xA0\u5F73\u4E01\u6B62\u6535\u961D\u5143"))), /*#__PURE__*/
+
+
+
+
+    React.createElement("section", { className: "hero" }, /*#__PURE__*/
+    React.createElement("img", {
+      src: "images/TwZAkQq.png",
+      alt: "\u80CC\u666F\u5716\u7247",
+      className: "hero-bg" }), /*#__PURE__*/
+
+    React.createElement("div", { className: "hero-overlay" }), /*#__PURE__*/
+    React.createElement("div", { className: "hero-content" }, /*#__PURE__*/
+    React.createElement("h2", { className: "hero-title" }, "2025 \u5168\u6C11\u88DC\u8CBC\u518D\u52A0\u78BC"), /*#__PURE__*/
+    React.createElement("p", { className: "hero-subtitle" }, "\u55AE\u8EAB\u5B69\u7AE5\u5F31\u52E2\u81F3\u5C11\u88DC\u52A9 3000"), /*#__PURE__*/
+    React.createElement("button", {
+      className: "hero-button",
+      onClick: scrollToAchievements }, "\u4E86\u89E3\u66F4\u591A"))), /*#__PURE__*/
+
+
+
+
+
+
+
+    React.createElement("section", { className: "achievements" }, /*#__PURE__*/
+    React.createElement("div", { className: "container" }, /*#__PURE__*/
+    React.createElement("h2", { className: "section-title" }, "2024 \u65BD\u653F\u6210\u679C"), /*#__PURE__*/
+    React.createElement("div", { className: "achievements-grid" },
+    [
+    {
+      title: "單身租金補貼",
+      description: "18~39歲單身者租金補貼加碼1.2億",
+      image: "images/iede_boy.png" },
+
+    {
+      title: "0~6 歲國家養",
+      description: "生 1  人x1.4倍，每多生一人再加碼0.2倍",
+      image: "images/friends_mamatomo2.png" },
+
+    {
+      title: "長者弱勢年金補助",
+      description: "65歲以上長者、身心障礙、災民等12類人加碼1.2倍",
+      image: "images/densya_silverseat.png" }].
+
+    map((item, index) => /*#__PURE__*/
+    React.createElement("div", { key: index, className: "achievement-card" }, /*#__PURE__*/
+    React.createElement("img", { src: item.image, alt: item.title }), /*#__PURE__*/
+    React.createElement("h3", null, item.title), /*#__PURE__*/
+    React.createElement("p", null, item.description)))))), /*#__PURE__*/
+
+
+
+
+
+
+
+    React.createElement("section", { className: "future-policy" }, /*#__PURE__*/
+    React.createElement("div", { className: "container" }, /*#__PURE__*/
+    React.createElement("h2", { className: "section-title" }, "2025 \u5E74\u88DC\u8CBC\u8FA6\u6CD5"), /*#__PURE__*/
+    React.createElement("p", { className: "policy-text" }, "\u55AE\u8EAB\u79DF\u91D1\u88DC\u8CBC\u30010~6 \u6B72\u570B\u5BB6\u990A\u3001\u9577\u8005\u5F31\u52E2\u5E74\u91D1\u88DC\u52A9", /*#__PURE__*/
+    React.createElement("br", null), "\u4F60\u4EE5\u70BA\u7684\u9019\u4E9B\u7D93\u8CBB\uFF0C\u5728\u672C\u5C46\u7ACB\u6CD5\u9662\u90FD\u88AB", /*#__PURE__*/
+    React.createElement("span", { class: "hightlight" }, "\u8CA1\u5283\u6CD5\u4FEE\u60E1"), "\u4FEE\u6389\u4E86\u3002", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/
+    React.createElement("img", { src: "images/money_single_mother_poor.png" })))), /*#__PURE__*/
+
+
+
+
+
+    React.createElement("section", { className: "slogan" }, /*#__PURE__*/
+    React.createElement("div", { className: "container" }, /*#__PURE__*/
+    React.createElement("h2", null, "\u4E82\u4FEE\u8CA1\u5283\u6CD5 \u798F\u5229\u6C92\u7D93\u8CBB"))), /*#__PURE__*/
+
+
+
+
+    React.createElement("section", { className: "videos" }, /*#__PURE__*/
+    React.createElement("div", { className: "container" }, /*#__PURE__*/
+    React.createElement("h2", { className: "section-title" }, "\u4E82\u4FEE\u8CA1\u5283\u6CD5\u8AAA\u660E\u5716\u5361"), /*#__PURE__*/
+    React.createElement("p", null, "\u8ACB\u4E0B\u8F09\u5206\u4EAB\u8F49\u50B3"), /*#__PURE__*/
+    React.createElement("div", { className: "videos-grid" },
+    [
+    'https://scontent.ftpe9-1.fna.fbcdn.net/v/t39.30808-6/471268816_1171430257680063_3091705327013465478_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=MjhUVjDeBvYQ7kNvgHI7c-g&_nc_zt=23&_nc_ht=scontent.ftpe9-1.fna&_nc_gid=AJtgo1SaH3F6-Qg2gA7J0Mu&oh=00_AYB0G_Qk-MeKjpAyue1gg0l7uKFSxV7rKn2Z60PyDxLbXA&oe=67712F25',
+    'https://scontent.ftpe9-1.fna.fbcdn.net/v/t39.30808-6/471499696_1125087012336175_1159454231533084347_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=ZJIVX7Ht7lgQ7kNvgE1nJHd&_nc_zt=23&_nc_ht=scontent.ftpe9-1.fna&_nc_gid=AOwZ1PjnwltLzrCs4DGzSZf&oh=00_AYDmBbnoGQeMZDZTmnU_t-YdrWqDr9WoNj17LHHj0SABCQ&oe=67714808',
+    'images/bZvt56f.png'].
+    map((img, index) => /*#__PURE__*/
+    React.createElement("div", { key: index, className: "video-container" }, /*#__PURE__*/
+    React.createElement("img", { src: img }))))))));
+
+
+
+
+
+
+
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
